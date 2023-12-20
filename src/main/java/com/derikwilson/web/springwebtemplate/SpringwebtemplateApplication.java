b68a99783835a29d1d5c82c82618f53ad59f2fb2
derikgw <derikwilson6@gmail.com>
2023-12-20 07:46:58 -0500

    Added pom and other classes

---
+++ src/main/java/com/derikwilson/web/springwebtemplate/SpringwebtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.derikwilson.web.springwebtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebtemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derikwilson/web/springwebtemplate/SpringwebtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derikwilson.web.springwebtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
